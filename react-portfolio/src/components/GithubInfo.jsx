c8cda86b3099440355c8144da2d628e6a9c00e8e
MLucky518 <56007160+MLucky518@users.noreply.github.com>
2020-12-07 00:26:12 -0500

    added some extra semantic components to help fix responsive styling

--- react-portfolio/src/components/GithubInfo.jsx
+++ react-portfolio/src/components/GithubInfo.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import GitHubCalendar from "react-github-calendar";
 
 function GithubInfo() {
-  const [userInfo, serUserInfo] = useState({});
+  const [userInfo, setUserInfo] = useState({});
 
   const getData = () => {
     axios
@@ -11,7 +11,7 @@ function GithubInfo() {
 
       .then((res) => {
         console.log(res);
-        serUserInfo(res.data);
+        setUserInfo(res.data);
         axios
           .get("https://api.github.com/users/MLucky518/repos")
 
@@ -30,22 +30,22 @@ function GithubInfo() {
 
   useEffect(() => {
     getData();
-    
   }, []);
 
   console.log(userInfo);
 
   return (
-    <div className = "github-container">
+    <div className="github-container">
       <h1>My GithubInfo</h1>
       <img src={userInfo.avatar_url}></img>
       <h1>{userInfo.login}</h1>
       <div className="calendar">
-      <GitHubCalendar 
-       color="purple" 
-       username={userInfo.login}
-       blockSize={14} />
-       </div>
+        <GitHubCalendar
+          color="purple"
+          username={userInfo.login}
+          blockSize={14}
+        />
+      </div>
     </div>
   );
 }
