9057d30940aa3fc1325b233b26b715a1085ffd88
MLucky518 <56007160+MLucky518@users.noreply.github.com>
2020-12-22 15:18:51 -0500

    fixed respnsive issues

--- react-portfolio/src/components/Projects.js
+++ react-portfolio/src/components/Projects.js
@@ -1,35 +1,17 @@
 import React from "react";
+import ProjectCard from "./ProjectCard";
 import { projectData } from "./util";
+import "./project.scss";
 
 
 function Projects() {
   return (
-    <div className="projects ">
+    <div className="projects">
       
       {projectData.map((project) => {
         return (
-          <div class="ui card project ">
-            <h1>{project.projectName}</h1>
-            <div class="image ">
-              <img src={project.img} />
-            </div>
-            <div style={{borderTop:"2px solid purple",width:"100%"}} class="content">
-              <div class="header">{project.projectName}</div>
-              <div class="meta">
-                <span class="date">{}</span>
-              </div>
-              <div class="description">
-                Matthew is a musician living in Nashville.
-              </div>
-            </div>
-            
-            <div class="extra content">
-              
-              <a>
-                <i aria-hidden="true" class="user icon"></i>22 Friends
-              </a>
-            </div>
-          </div>
+          
+          <ProjectCard project = {project}/>
         );
       })}
     </div>
