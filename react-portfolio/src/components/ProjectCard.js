4d42134fcfbe263752d9cdbc51c496feaef080e1
MLucky518 <56007160+MLucky518@users.noreply.github.com>
2021-02-22 17:37:53 -0500

    fixed project card styles/ removed redundant old skill styles

--- react-portfolio/src/components/ProjectCard.js
+++ react-portfolio/src/components/ProjectCard.js
@@ -2,34 +2,36 @@ import React from "react";
 import { Card, Icon, Image } from "semantic-ui-react";
 
 function ProjectCard(props) {
-  console.log(props);
-  return (
-    <Card className="project proj">
-      <Image
-        style={{ borderBottom: "2px solid purple" }}
-        src={props.project.img}
-        wrapped
-        
-        class = "project-pic"
-      />
-      <Card.Content>
-        <Card.Header>
-          <h1 className = "project-header">{props.project.projectName}</h1>
-        </Card.Header>
-        <Card.Meta>
-          <span className="date">{props.project.techUsed}</span>
-        </Card.Meta>
-        <Card.Description>{props.project.textContent}</Card.Description>
-      </Card.Content>
-      <Card.Content className="project-links" extra>
-        <p>
-          Github - <a href={props.project.github}>{props.project.github}</a>{" "}
-        </p>
-        <br/>
-        <p>Deployed Site - <a href = {props.project.liveSite}>{props.project.liveSite}</a></p>
-      </Card.Content>
-    </Card>
-  );
+    console.log(props);
+    return (
+        <Card className="project proj">
+            <Image src={props.project.img} wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>
+                    <h3 className="project-header">
+                        {props.project.projectName}
+                    </h3>
+                </Card.Header>
+                <Card.Meta>
+                    <span className="date">{props.project.techUsed}</span>
+                </Card.Meta>
+                <Card.Description>{props.project.textContent}</Card.Description>
+            </Card.Content>
+            <Card.Content className="project-links" extra>
+                <p>
+                    Github -{" "}
+                    <a href={props.project.github}>{props.project.github}</a>{" "}
+                </p>
+                <br />
+                <p>
+                    Deployed Site -{" "}
+                    <a href={props.project.liveSite}>
+                        {props.project.liveSite}
+                    </a>
+                </p>
+            </Card.Content>
+        </Card>
+    );
 }
 
 export default ProjectCard;
