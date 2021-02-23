5d1953254bba6552b2781e594fd585550c79984b
MLucky518 <56007160+MLucky518@users.noreply.github.com>
2021-02-22 19:35:11 -0500

    fixes 4

--- react-portfolio/src/components/AboutTransition.js
+++ react-portfolio/src/components/AboutTransition.js
@@ -1,34 +1,30 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button, Divider, Image, Transition } from "semantic-ui-react";
 import About from "./About";
 import { aboutInfo } from "./util";
 
-export default class AboutTransition extends Component {
-  state = { visible: false };
-
-  toggleVisibility = () =>
-    this.setState((prevState) => ({ visible: !prevState.visible }));
-
-  render() {
-    
+export default function AboutTransition() {
+    const [state, setState] = useState(false);
+    const toggleVisibility = () => {
+        setState(!state);
+    };
 
     return (
-      <div className="about-contents">
-        <Button
-          content={this.state.visible ? "X" : "About Me"}
-          onClick={this.toggleVisibility}
-          className={"about-button"}
-        />
-        <Divider hidden />
-        <Transition
-          visible={this.state.visible}
-          animation="scale"
-          duration={500}
-          unmountOnHide={true}
-        >
-          <About info={aboutInfo} header={"Hello World,"} />
-        </Transition>
-      </div>
+        <div className="about-contents">
+            <Button
+                content={state ? "X" : "About Me"}
+                onClick={toggleVisibility}
+                className={"about-button"}
+            />
+            <Divider hidden />
+            <Transition
+                visible={state}
+                animation="scale"
+                duration={500}
+                unmountOnHide={true}
+            >
+                <About info={aboutInfo} header={"Hello World,"} />
+            </Transition>
+        </div>
     );
-  }
 }
